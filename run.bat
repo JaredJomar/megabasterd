a3303c1be0335c430d6b6177698397aca9447239
JaredJomar <jared.cruz@upr.edu>
2025-07-08 16:18:45 -0400

    Add run.bat script to execute MegaBasterd application

---
+++ run.bat
@@ -0,0 +1,5 @@
+REM Change directory to the target folder
+cd "b:\Documents\Github\Personals-Projects\Projects\megabasterd\target"
+
+REM Run the MegaBasterd application JAR file
+java -jar MegaBasterd-8.22-jar-with-dependencies.jar
