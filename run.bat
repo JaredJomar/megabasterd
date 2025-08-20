1b264a874628ca9b75b58538b8e3692696993333
JaredJomar <jared.cruz@upr.edu>
2025-08-19 20:09:45 -0500

    Remove run.bat and test_proxifly_api.bat scripts

--- run.bat
+++
@@ -1,5 +0,0 @@
-REM Change directory to the target folder
-cd "b:\Documents\Github\Personals-Projects\Projects\megabasterd\target"
-
-REM Run the MegaBasterd application JAR file
-java -jar MegaBasterd-8.22-jar-with-dependencies.jar
